35bc0ed5d16191fc4b49219b9809783b29c82088
Anuj Shrivastav <anujshrivastav97@gmail.com>
2019-03-26 19:51:06 +0530

    added users table in database ;

--- complaint_handler.php
+++ complaint_handler.php
@@ -6,15 +6,17 @@ if(!isset($_SESSION['sid']))
   }
 $id=$_SESSION['sid'];
 $hostelno=$_POST['hostelno'];
-$category=$_POST['complaint'];
+$category=$_POST['Complaint'];
 $description=$_POST['message'];
-$q="insert into complaints(category,description,hostelno) values ('$category','$description','$hostelno');";
 $con=mysqli_connect('localhost','root');
 mysqli_select_db($con,'Hostel');
-mysqli_query($con,$q);
 $resid=mysqli_query($con,"select cid from complaints;");
 $num=mysqli_num_rows($resid);
-$nextid=$num;
+$nextid=$num+1;
+$q="insert into complaints(cid,category,description,hostelno) values ($nextid,'$category','$description','$hostelno');";
+
+mysqli_query($con,$q);
+
 $q2="insert into writes_com(sid,cid) values($id,$nextid)";
 $final=mysqli_query($con,$q2);
 mysqli_close($con);
