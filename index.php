d65975a61f3a12c8fb63b7c37c0042dac4044ff6
Anuj Shrivastav <anujshrivastav97@gmail.com>
2019-03-26 15:49:27 +0530

    done changes

--- index.php
+++ index.php
@@ -6,21 +6,33 @@ session_start();
 <head>
 	<title>MANIT HOSTEL</title>
 	<link type="text/css" rel="stylesheet" href="index.css">
-	
+	<style type="text/css">
+		nav a {
+			color:white;
+		}
+
+		nav li {
+			color:white;
+		}
+
+		
+
+
+	</style>
 </head>
 <body>
 <nav>
-	<span style="color:white;">HOSTEL MANAGEMENT SYSTEM V2.0</span>
+	<span style="color:white;"><a href="index.php">HOSTEL MANAGEMENT SYSTEM V2.0</a></span>
 	<ul>
 		<?php if(isset($_SESSION['sid'])) {?>
 		<li>Dashboard</li>
 		<li>Room Allotment</li>
 		<li>Review a Room</li>
-		<li><a href="complaint.php">Complaints</a></li>
+		<li><a  href="complaint.php">Complaints</a></li>
 	<?php } ?>
 		<?php if(isset($_SESSION['sid'])) { ?>
 		<li><?php echo "".$_SESSION['sid'] ?> <a href="logout.php">Logout</a></li>
-		<?php } else { ?><li><a href="login.php">Login </a></li><li><a href="register.php">Register </a></li> <?php } ?>
+		<?php } else { ?><li><a  href="login.php">Login </a></li><li><a href="register.php">Register </a></li> <?php } ?>
 	</ul>
 </nav>
 <div>
