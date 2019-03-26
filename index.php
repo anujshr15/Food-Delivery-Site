24eb42859dc5165d0f296ccf702c9c7531796930
Anuj Shrivastav <anujshrivastav97@gmail.com>
2019-03-26 13:40:31 +0530

    added register link to navbar;

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ session_start();
 	<?php } ?>
 		<?php if(isset($_SESSION['sid'])) { ?>
 		<li><?php echo "".$_SESSION['sid'] ?> <a href="logout.php">Logout</a></li>
-		<?php } else { ?> <li><a href="login.php">Login </a></li> <?php } ?>
+		<?php } else { ?><li><a href="login.php">Login </a></li><li><a href="register.php">Register </a></li> <?php } ?>
 	</ul>
 </nav>
 <div>
