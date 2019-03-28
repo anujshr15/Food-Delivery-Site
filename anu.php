23224c1811915f9f49a4680845fec36adf992665
Anu1399 <ananyamail.2013@gmail.com>
2019-03-28 19:43:54 +0530

    'message'

--- anu.php
+++ anu.php
@@ -3,17 +3,25 @@
 <head>
 	<title>ANU</title>
 	<style type="text/css">
+	*{
+	margin: 0px ;
+	padding: 0px ;
+	box-sizing: border-box;
+}
+
 
 		body{
 			background-color: powderblue ;
+			
+
 		}
 		h1{
-	 width: 1180px ;
+	 width: 100%;
 	position: absolute;
-	top: 5% ;
-	left: 50% ;
-	font-size: 250% ;
-	transform: translate(-50%,-50%);
+	margin-top: 5px ;
+	margin-left: 80px ;
+	font-size: 180% ;
+	
 	color: black ;
 	letter-spacing: 3px ;
 	word-spacing: 5px ;
@@ -21,27 +29,30 @@
 }
      .main{
          
-         margin-top: 320px ;
+         margin-top: 20px ;
 
-         background-color: yellow ;
+         /*background-color: yellow ;*/
      }
+       
+    
+
 		
 
 	</style>
 
 </head>
 <body> 
-	<h1>REVIEW SUBMISSION</h1>
-	<img src="hostel_1.jpg" width="33%" height="220px">
+	<h1>MAULANA AZAD NATIONAL INSTITUTE OF TECHNOLOGY</h1>
+	<img src="3.jpg" width="100%" height="220px" class="logo" style="margin-top:80px ;">
 	<div class="main">
-	<form action="review.php" method="POST" style="margin: auto;width: 30%;text-align: center;height: 250px;">
+	<form action="review.php" method="POST"  style="margin-top:5px ;width: 50%;text-align: center;height: 250px; margin-left: 25%">
 
 		<fieldset>
 			<legend style="text-align: center">REVIEW</legend>
 		ROOM:<br><input type="text" name="room"><br>
 		NAME:<br><input type="text" name="student"><br>
 		SCHOLAR_NO:<br><input type="number" name="rno"><br>
-		REVIEW:<br><input type="text" name="rev"><br>
+		REVIEW:<br><input type="text" name="rev" style="width:90% ; height:100px ; "><br>
 		<input type="submit">
 		 </fieldset>
 
