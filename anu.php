2b8c175b0029dbb1902ad412df3c85f46a51721b
Anu1399 <ananyamail.2013@gmail.com>
2019-03-29 10:02:15 +0530

    'newrev'

--- anu.php
+++ anu.php
@@ -3,11 +3,8 @@
 <head>
 	<title>ANU</title>
 	<style type="text/css">
-	*{
-	margin: 0px ;
-	padding: 0px ;
-	box-sizing: border-box;
-}
+
+	
 
 
 		body{
@@ -15,35 +12,21 @@
 			
 
 		}
-		h1{
-	 width: 100%;
-	position: absolute;
-	margin-top: 5px ;
-	margin-left: 80px ;
-	font-size: 180% ;
-	
-	color: black ;
-	letter-spacing: 3px ;
-	word-spacing: 5px ;
-	font-weight: lighter;
-}
-     .main{
-         
-         margin-top: 20px ;
-
-         /*background-color: yellow ;*/
-     }
+		
+     
        
     
 
 		
 
 	</style>
+	<link rel="stylesheet" type="text/css" href="nav.css">
 
 </head>
 <body> 
-	<h1>MAULANA AZAD NATIONAL INSTITUTE OF TECHNOLOGY</h1>
-	<img src="3.jpg" width="100%" height="220px" class="logo" style="margin-top:80px ;">
+	<header>
+		<?php include 'navbar.php'?>
+	</header>
 	<div class="main">
 	<form action="review.php" method="POST"  style="margin-top:5px ;width: 50%;text-align: center;height: 250px; margin-left: 25%">
 
