da69b6bd37d1d11b411f624a461c0fcfe66ee94a
trilok98 <trilokahirwar21@gmail.com>
2019-03-29 21:35:57 +0530

    bla bla bla

--- navbar.php
+++ navbar.php
@@ -8,7 +8,7 @@
 <body>
 <header style="text-align: center;">
 <nav style="margin-top: -25px;">
-	<img src="manitlogo.png" style="width:auto; height: 140px; float: left;padding-top: 20px;padding-left: 20px;">
+	<img src="manitlogo.png" style="width:140px; height: 140px; float: left;padding-top: 20px;padding-left: 20px;">
     
 	<h2 style="text-align: center;padding-top: 16px;word-spacing: 3px;letter-spacing: 1px;text-shadow: 2px 2px red;"><a href="index.php" style="color: yellow;">Hostel Management System</a></h2>
 	<h4 style="color: yellow;text-align: center;word-spacing: 3px;letter-spacing: 1px;padding-top: 0px;">Maulana Azad National Institute of Technology, Bhopal</h4>
