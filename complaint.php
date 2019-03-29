9c02da12be7b45aeb29cf5808f3ec3a3dc2abe7e
Anu1399 <ananyamail.2013@gmail.com>
2019-03-29 22:20:46 +0530

    'message'

--- complaint.php
+++ complaint.php
@@ -136,11 +136,25 @@ textarea
 </div>
 <br>
 <br>
-<footer>
-<pre>
- Link Road No 3,Near Kali Mata Mandir,Bhopal 462003 |  <a href="http://www.manit.ac.in/" style="color: yellow;">www.manit.ac.in</a>  | Telephone: +91 755 4051000,4052000,2670900 | Fax: +91-755 2670562
-</pre>
+<footer style="margin-top: 300px;text-align: center;background-color: black;padding-top: 20px;color: white;padding-bottom: 10px;">
+  <a href="https://www.facebook.com/MANIT-Bhopal-217608298830555/">
+<img src="facebook.png" width="40" height="30">
+</a>
+<a href="https://twitter.com/manitbpl" >
+  <img src="twitter.png" width="30" height="30">
+</a>
+<a href="https://www.youtube.com/channel/UCXzZC99puUZuJDiQ09p72cw?reload=9">
+  <img src="youtube.png" width="50" height="30">
+</a>
+<a href="https://plus.google.com/u/0/103957241353925049032"></a>
+<img src="googleplus.png" width="34" height="30"><br>
+Copyright 2019
 </footer>
+<!-- <script>
+    var par=document.querySelector("footer");
+    var dt=new Date();
+    par.innerText+=" "+(dt.getYear()+1900).toString();
+  </script> -->
 
 </body>
 
--- review.php
+++ review.php
@@ -15,42 +15,68 @@ $_SESSION['url']=$_SERVER['REQUEST_URI'];
 	<title>Thank You!!</title>
 	<style type="text/css">
 		body{
-			background-color:yellow ;
+			background-color: powderblue ;
 			
-
 
 		}
 
 	
 
  .re{
-  width: 300px;
+  width: 700px;
   border: 5px solid grey;
   padding: 25px;
   margin: 25px;
-  margin-left: 33% ;
-  border-radius: 25px;
+  margin-left: 20% ;
+  border-radius: 25px
+  font color: red ;
+  background-color: green ;
+  height: 200px ;
+  background-image: url(pen4.jpg) ;
+  text-decoration-color: red ;
+
 }
  .me{
- 	margin-left: 62% ;
+ 	margin-left: 75% ;
+ 	font-size: 30px ;
  }
-  h2{
-  	font-size: 120%
-  	text-align  
+  .hey{
+  	width: 70px;
+  /*border: 1px solid grey;*/
+  padding: .5px;
+  margin: .5px;
+  margin-left: 45% ;
+  border-radius: 25px;
+  text-decoration: underline;
+
+  
   }
+  body
+{
+  margin:0;
+  background-color: black;
+}
+
+      
+
+    #grad1 
+{
+  background-image: linear-gradient(to right,rgba(60,120,255,.5),rgba(60,120,255,.5),rgba(60,120,255,0.5));
+}
 	</style>
 	<link rel="stylesheet" type="text/css" href="nav.css">
 </head>
-<body>
+<body id="grad1"> 
 	<div class="head">
 	<header>
 
 		<?php include 'navbar.php'?>
 	</header>
-	<h2><?php echo $_POST["room"] ;?></h2>
-  <div class="re"><em>
-  <?php echo $_POST["rev"] ; ?></em></div>
-  <div class="me">by <?php  echo $_POST["student"] ; ?></div>
+	<div class="hey"><h2><?php echo $_POST["room"] ;?></h2></div>
+  <div class="re" ><em>
+  <?php echo $_POST["rev"] ; ?><br>By<br><?php  echo $_POST["student"] ; ?><br><?php  echo $_POST["rno"] ; ?></em></div>
+  
+  </div>
 
   <!-- <footer>@Copyright 2019</footer> -->
 <footer style="margin-top: 300px;text-align: center;background-color: black;padding-top: 20px;color: white;padding-bottom: 10px;">
